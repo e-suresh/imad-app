e584cbfb02ed69ab352ca3b6c064cbe7e642b036
e-suresh <31181222+e-suresh@users.noreply.github.com>
2017-09-06 21:27:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {}
-);res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+);res.sendFile(path.join(__dirname, 'ui', 'index.html')
+
+app.get('/')
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
